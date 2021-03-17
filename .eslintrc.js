fff89b6cc7675111116e367e6ba1fedaf692ad56
noelsev <noel.sev@gmail.com>
2021-03-17 23:37:33 +1300

    disable strict any from lint and run tests sequentially

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,8 @@ module.exports = {
         'semi': [
             'error',
             'always'
-        ]
+        ],
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off'
     }
 };
