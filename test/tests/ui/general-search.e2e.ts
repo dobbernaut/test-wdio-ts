bbb2f638a720ee499a087cc32bcf48998f1686af
noelsev <noel.sev@gmail.com>
2021-03-19 09:00:30 +1300

    add tests for search and watchlist

---
+++ test/tests/ui/general-search.e2e.ts
@@ -0,0 +1,21 @@
+import { Categories } from '../../enums/listing-categories';
+import { Antiques } from '../../enums/listing-sub-categories';
+import { Home } from '../../pages/home';
+import { Listings } from '../../pages/listings';
+
+describe('Search', () => {
+
+  const home = new Home();
+  const listings = new Listings();
+
+  it('should return the expected number of listings', () => {
+    home.open();
+    home.openMainCategory(Categories.antiques);
+    home.openSubcategory(Antiques.stamps);
+    home.search('product');
+
+    expect(listings.getListings().length).toEqual(12);
+    expect(listings.totalListings()).toEqual(12);
+  });
+
+});
---
+++ test/tests/ui/watchlist.e2e.ts
@@ -0,0 +1,42 @@
+import { roles } from '../../config';
+import { Categories } from '../../enums/listing-categories';
+import { Antiques } from '../../enums/listing-sub-categories';
+import { Home } from '../../pages/home';
+import { Listings } from '../../pages/listings';
+import { Watchlist } from '../../pages/watchlist';
+import { Listing } from '../../types/listing';
+
+describe('Watchlist', () => {
+
+  const home = new Home();
+  const listings = new Listings();
+  const watchlist = new Watchlist();
+
+  let listing: Listing;
+
+  beforeAll(() => {
+    home.logIn(roles.testRole);
+    home.open();
+  });
+
+  afterAll(() => {
+    watchlist.removeListingsFromWatchlist([listing]);
+    home.logOut();
+  });
+
+  it('should add a listing to the user watchlist', () => {
+    home.openMainCategory(Categories.antiques);
+    home.openSubcategory(Antiques.stamps);
+    home.search('product');
+    listing = listings.getListings()[0];
+
+    listings.addListingToWatchlist({listingId: listing.listingId});
+    home.viewWatchlist();
+    const watchlistListingIds: number[] = watchlist.getListingsOnWatchlist().map(watchlistListing => {
+      return watchlistListing.listingId;
+    });
+
+    expect(watchlistListingIds).toContain(listing.listingId);
+  });
+
+});
